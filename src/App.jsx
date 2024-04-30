fd3c1f3b06e2f287c2048dc393d2b7090885b120
John Wesley Baki <johnwesleybaki3@gmail.com>
2024-04-30 23:50:02 +0530

    added spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-// 
+
+
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AnimeList from './AnimeList';
 import AnimePage from './AnimePage';
-import  './App.css';
-
+import './App.css';
 
 function App() {
   const [animes, setAnimes] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [sortBy, setSortBy] = useState("");
   const [showSearchAndSort, setShowSearchAndSort] = useState(true);
-  
+  const [loading, setLoading] = useState(false); 
+
   // Function to fetch anime data
   const getAnime = async (name) => {
     try {
+      setLoading(true); // Set loading state to true when fetching data
       const response = await fetch(`https://api.jikan.moe/v4/anime?q=${name}`);
       const data = await response.json();
+      setLoading(false); // Set loading state to false when data fetching is done
       return data.data;
     } catch (error) {
+      setLoading(false); // Set loading state to false in case of error
       console.error('Error fetching anime data:', error);
       return [];
     }
@@ -35,21 +39,12 @@ function App() {
     setSortBy(event.target.value);
   };
 
-  const handleFilterChange = (event) => {
-    setFilterBy(event.target.value);
-  };
-
-
-
-  
-useEffect(() => {
-  // Fetch anime data and update state for initial load
-  getAnime("Naruto")
-    .then(data => 
-      setAnimes(data))
-    .catch(error => console.error('Error fetching anime data:', error));
-}, []);
-
+  useEffect(() => {
+    // Fetch anime data and update state for initial load
+    getAnime("Naruto")
+      .then(data => setAnimes(data))
+      .catch(error => console.error('Error fetching anime data:', error));
+  }, []);
 
   // sort the anime list based on selected criteria
   let SortedAnimes = [...animes];
@@ -60,52 +55,55 @@ useEffect(() => {
         return a.title.localeCompare(b.title);
       } else if (sortBy === "ranking") {
         return a.rank - b.rank;
-      }else if (sortBy === "popularity") {
+      } else if (sortBy === "popularity") {
         return a.popularity - b.popularity;
       }
     });
   }
 
   return (
-
     <Router>
-      
       <div className='container'>
-      {showSearchAndSort && (
+        {showSearchAndSort && (
           <>
-        <input
-          type="text"
-          placeholder="Search for anime e.g. Naruto"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button onClick={handleSearch}>Search</button>
-
-
-          <div className="select-container">
-            <label htmlFor="sortBy" className="select-label">Sort By:</label>
-            <select id="sortBy" className="select" value={sortBy} onChange={handleSortChange}>
-              <option value="">None</option>
-              <option value="title">Title</option>
-              <option value="ranking">Ranking</option>
-              <option value="popularity">Popularity</option>
-            </select>
-            <span className="select-arrow">&#9660;</span>
-          </div>
-
+            <input
+              type="text"
+              placeholder="Search for anime e.g. Naruto"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+            <div className="select-container">
+              <label htmlFor="sortBy" className="select-label">Sort By:</label>
+              <select id="sortBy" className="select" value={sortBy} onChange={handleSortChange}>
+                <option value="">None</option>
+                <option value="title">Title</option>
+                <option value="ranking">Ranking</option>
+                <option value="popularity">Popularity</option>
+              </select>
+              <span className="select-arrow">&#9660;</span>
+            </div>
           </>
         )}
+      </div>
 
-        </div>
-        
+      {loading ? (
+     <div className="loader">
+     <div className="spinner"></div>
+     <p>Loading...</p>
+   </div>
+   
+    
+     ): (
         <Routes>
           <Route
             path="/"
             element={<AnimeList animes={SortedAnimes} setShowSearchAndSort={setShowSearchAndSort} />}
           />
-          <Route path="/anime/:id" element={<AnimePage setShowSearchAndSort={setShowSearchAndSort}/>} />
+          <Route path="/anime/:id" element={<AnimePage setShowSearchAndSort={setShowSearchAndSort} />} />
         </Routes>
-      
+      )}
+
     </Router>
   );
 }
